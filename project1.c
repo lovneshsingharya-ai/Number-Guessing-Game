b950ed5a3d3efa8c36d30bdd349091a4fcade12e
Lovnesh Singh Arya <lovneshsingharya@gmail.com>
2026-09-05 22:40:46 +0530

    first commit

---
+++ project1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(0));
+    int randomNumber = (rand() %100)+1;
+    int no_of_guesses = 0;
+    int guessed_number;
+
+    do
+    { 
+        printf("guess the number");
+        scanf("%d", &guessed_number);
+        if(guessed_number >randomNumber){
+            printf("Lower number please\n");
+        }
+        else if (guessed_number <randomNumber){
+            printf("Higher number please \n");
+        }
+        else {
+            printf("congurulations!!\n");
+        }
+        no_of_guesses++;
+    }
+    while (guessed_number != randomNumber);
+    printf("you guessed the number in %d gusses",no_of_guesses);
+    return 0;
+}
